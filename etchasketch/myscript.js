47a18e0d27c01368566c078d2b6505748e90fc4d
Tyler Kleizo <takleizo@gmail.com>
2016-03-21 23:12:34 -0700

    3/21

---
+++ etchasketch/myscript.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    for(i=0;i<4;i++)
+    {
+        $('table').append('<tr></tr>');
+    }
+    
+    for(x=0;x<4;x++)
+    {
+        $('tr').append('<td>x</td>');
+    }
+    
+});
+
+function newGrid() {
+
+};
