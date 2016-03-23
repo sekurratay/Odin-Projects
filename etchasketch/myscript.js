b1394aa9aa238e7850603382e7657f1c1529446f
Tyler Kleizo <takleizo@gmail.com>
2016-03-22 23:52:29 -0700

    got the resize working

--- etchasketch/myscript.js
+++ etchasketch/myscript.js
@@ -1,17 +1,29 @@
-$(document).ready(function() {
-    for(i=0;i<4;i++)
+var defSize=4;
+
+$(document).ready(function(){
+    createGrid(defSize);
+});
+
+$('.resize').click(function(){
+        newGrid();
+});
+
+function createGrid(size){
+    for(i=0;i<size;i++)
     {
         $('table').append('<tr></tr>');
     }
     
-    for(x=0;x<4;x++)
+    for(x=0;x<size;x++)
     {
-        $('tr').append('<td>x</td>');
+        $('tr').append('<td class="square"></td>');
     }
-    
-    
-});
+    var width = $('table').width()/size;
+    $('.square').css({"width":width,"height":width});
+};
 
 function newGrid() {
-
-};
+    $('.square').remove();
+    var c = parseInt(prompt("Enter a size (1-100): ",10),10);
+    createGrid(c);
+}
